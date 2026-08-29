6c567681f113d4bf6c4d5b0b132882f114ffca35
giovannisouzal <giovannisouzal2005@gmail.com>
2026-08-29 12:25:53 -0300

    estrutura do código cabecalho e o seu module criado

---
+++ cp4/src/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import styles from './Cabecalho.module.css'
+import type { CabecalhoProps } from '../types'
+
+function Cabecalho({ titulo, logo }: CabecalhoProps) {
+  useEffect(() => {
+    document.title = titulo
+  }, [titulo])
+
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logo da FIAP" className={styles.logo} />
+      <h1>{titulo}</h1>
+    </header>
+  )
+}
+
+export default Cabecalho
